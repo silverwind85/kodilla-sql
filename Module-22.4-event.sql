dd4b5b8be276455e4d4b91552063ec3b33aa5441
“silverwind85 <slcieslar@gmail.com>
2018-02-05 15:52:19 +0100

    First commit event module-22.5

---
+++ Module-22.4-event.sql
@@ -0,0 +1,31 @@
+create table stats(
+	stat_id int(11) not null auto_increment,
+    stat_date datetime not null,
+    stat varchar(20) not null,
+    value int(11) not null,
+    primary key(stat_id)
+    );
+    
+ 
+create view bestsellers_count as
+ select count(bestseller) as bestsellers_count from books where bestseller=1;
+ select * from books;
+ 
+
+ delimiter |
+ create event Update_stat
+	on schedule every 1 minute
+    do begin 
+		call UpdateBestSellers();
+		insert into stats(stat_date,stat,value)
+		values(curdate(),'bestsellers',(select bestsellers_count from bestsellers_count));
+	end|
+    delimiter ;
+    
+ select * from stats;
+ show processlist; 
+ drop event Update_stat;
+ select bestsellers_count from bestsellers_count;
+ 
+ 
+ 
