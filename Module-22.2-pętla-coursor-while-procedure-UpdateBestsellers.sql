9cf1c6469ecc50e0965c53c8b4bec1fbb1731d05
“silverwind85 <slcieslar@gmail.com>
2018-02-05 16:07:26 +0100

    second commit module-22.2

--- Module-22.2-pętla-coursor-while-procedure-UpdateBestsellers.sql
+++ Module-22.2-pętla-coursor-while-procedure-UpdateBestsellers.sql
@@ -4,28 +4,25 @@ delimiter $$
 
 create procedure UpdateBestSellers()
 begin 
-	declare booksread, days, rdr_id int ;
+	declare booksread, days, bk_id int ;
     declare bookspermonth decimal(5,2);
     declare finished int default 0;
-    declare all_readers cursor for select book_id from books;
+    declare all_books cursor for select book_id from books;
     declare continue handler for not found set finished = 1;
-    open all_readers;
-    while(finished = 0) Do
-    fetch all_readers into rdr_id;
-    if (finished =0) then
-    select count(*) from rents
-    where book_id = rdr_id
-    into booksread;
-    select datediff(max(rent_date), min(rent_date)) from rents
-    where book_id = rdr_id
-    into days;
-    set bookspermonth = booksread/days*30;
-    update books set bestseller = BestsellerBook(bookspermonth)
-    where book_id = rdr_id;
-    commit;
-    end if;
+    
+    open all_books;
+    
+	while(finished = 0) Do
+		fetch all_books into bk_id;
+		if (finished =0) then
+			select count(*) from rents where book_id = bk_id into booksread;
+			select datediff(max(rent_date), min(rent_date)) from rents where book_id = bk_id into days;
+			set bookspermonth = booksread/days*30;
+			update books set bestseller = BestsellerBook(bookspermonth) where book_id = bk_id;
+			commit;
+		end if;
     end while;
-    close all_readers;
+    close all_books;
 end $$
 
 delimiter ;
